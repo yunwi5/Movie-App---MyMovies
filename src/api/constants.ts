d8a363df8c904e687c4983ee9d351ac84d1bf160
Yunkeun <1009jyk@gmail.com>
2022-04-09 12:01:32 +1200

    Env variables added. User APIs refactored.

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,3 +1,3 @@
-export const API_KEY = process.env.REACT_APP_API_KEY || '';
+export const API_KEY = process.env.REACT_APP_FIREBASE_API_KEY || '';
 export const FIREBASE_DOMAIN = process.env.REACT_APP_FIREBASE_DOMAIN || '';
 export const STORE_MOVIES_KEY = process.env.REACT_APP_STORE_MOVIES_KEY || '';
