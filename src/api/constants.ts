1e3221610f45b6e48eb06de04e85dc06ac3db074
Yunkeun <1009jyk@gmail.com>
2022-05-22 06:38:24 +1200

    updated.

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,3 +1,3 @@
-export const API_KEY = 'AIzaSyDTZ0QbMDjcR020EQXdsQ7KSOcfJwPT7vQ';
-export const FIREBASE_DOMAIN = 'https://react-movie-app-336f6-default-rtdb.firebaseio.com';
+export const API_KEY = process.env.REACT_APP_FIREBASE_API_KEY || '';
+export const FIREBASE_DOMAIN = process.env.REACT_APP_FIREBASE_DOMAIN || '';
 export const STORE_MOVIES_KEY = '-MuxOnh92osEeOMsMEre';
